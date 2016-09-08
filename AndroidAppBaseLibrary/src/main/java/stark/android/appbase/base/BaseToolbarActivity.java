191808a4868181f0e114d125e209ad7f9c4d3a78
jihongwen <jhwing@163.com>
2016-09-08 15:54:58 +0800

    update

--- AndroidAppBaseLibrary/src/main/java/stark/android/appbase/base/BaseToolbarActivity.java
+++ AndroidAppBaseLibrary/src/main/java/stark/android/appbase/base/BaseToolbarActivity.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.Toolbar;
 
+import stark.android.appbase.R;
+
 /**
  * Created by jihongwen on 16/8/4.
  */
@@ -15,7 +17,7 @@ public abstract class BaseToolbarActivity<T> extends BaseActivity<T> {
     @Override
     protected void onPostCreate(@Nullable Bundle savedInstanceState) {
         super.onPostCreate(savedInstanceState);
-        toolbar = (Toolbar) findViewById(getToolbarId());
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
         toolbar.setTitle("");
         setSupportActionBar(toolbar);
         setToolbar(toolbar);
