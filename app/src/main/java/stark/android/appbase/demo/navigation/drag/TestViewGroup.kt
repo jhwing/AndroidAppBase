1e007e971a91723f53fe1581a0be283bb2ff44b5
jihongwen <jhwing@163.com>
2018-08-02 15:42:43 +0800

    弹窗底部view，按钮样式渐变

--- app/src/main/java/stark/android/appbase/demo/navigation/drag/TestViewGroup.kt
+++ app/src/main/java/stark/android/appbase/demo/navigation/drag/TestViewGroup.kt
@@ -1,11 +1,15 @@
 package stark.android.appbase.demo.navigation.drag
 
 import android.content.Context
+import android.support.v4.view.ViewCompat
 import android.support.v4.widget.ViewDragHelper
 import android.util.AttributeSet
+import android.util.Log
 import android.view.MotionEvent
 import android.view.View
+import android.view.ViewGroup
 import android.widget.FrameLayout
+import stark.android.appbase.demo.R
 
 /**
  * 使用 ViewDragHelper
@@ -14,20 +18,77 @@ class TestViewGroup @JvmOverloads constructor(
         context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
 ) : FrameLayout(context, attrs, defStyleAttr) {
 
+    private var drawPanel: View? = null
+    private var testDrawView3: View? = null
+    private var slidingUpPanel: View? = null
+    private var openBtn: View? = null
+    private var closeBtn: View? = null
+
+    enum class PanelState {
+        EXPLODED, HIDDEN
+    }
+
+    var panelState = PanelState.HIDDEN
+
+    override fun onFinishInflate() {
+        super.onFinishInflate()
+        openBtn = findViewById(R.id.openBtn)
+        closeBtn = findViewById(R.id.closeBtn)
+        closeBtn?.alpha = 0f
+        drawPanel = findViewById(R.id.drawPanel)
+        testDrawView3 = findViewById(R.id.testDrawView3)
+        slidingUpPanel = findViewById(R.id.slidingUpPanel)
+        drawPanel?.setOnClickListener {
+            if (panelState == PanelState.HIDDEN) {
+                panelState = PanelState.EXPLODED
+                smoothSlideTo(0)
+            } else {
+                panelState = PanelState.HIDDEN
+                smoothSlideTo(oriSlidingUpTop)
+            }
+        }
+    }
+
+    var oriSlidingUpTop = 0
+
+    override fun onLayout(changed: Boolean, left: Int, top: Int, right: Int, bottom: Int) {
+        super.onLayout(changed, left, top, right, bottom)
+        if (drawPanel != null) {
+            if (panelState == PanelState.HIDDEN) {
+                val parentHeight = bottom - top
+                val height = drawPanel!!.height
+                oriSlidingUpTop = bottom - height
+                slidingUpPanel?.layout(left, oriSlidingUpTop, right, oriSlidingUpTop + parentHeight)
+            }
+        }
+    }
+
+    // 展开和收起
+
     var mDragOriLeft = 0
     var mDragOriTop = 0
 
     private val callback = object : ViewDragHelper.Callback() {
         override fun tryCaptureView(child: View, pointerId: Int): Boolean {
-            return true
+            val tag = child.tag
+            return "drawView" == tag
         }
 
         override fun clampViewPositionHorizontal(child: View, left: Int, dx: Int): Int {
+            Log.d("jihongwen", "clampViewPositionHorizontal left=$left dx=$dx")
             return left
         }
 
         override fun clampViewPositionVertical(child: View, top: Int, dy: Int): Int {
+            Log.d("jihongwen", "clampViewPositionVertical top=$top dy=$dy")
             return top
+        }
+
+        override fun onViewPositionChanged(changedView: View, left: Int, top: Int, dx: Int, dy: Int) {
+            super.onViewPositionChanged(changedView, left, top, dx, dy)
+            Log.d("jihongwen", "left=$left top=$top dx=$dx dy=$dy")
+            changeBtn(top)
+            changedView.layoutParams.height = ViewGroup.LayoutParams.MATCH_PARENT
         }
 
         override fun onViewCaptured(capturedChild: View, activePointerId: Int) {
@@ -40,6 +101,7 @@ class TestViewGroup @JvmOverloads constructor(
             super.onViewReleased(releasedChild, xvel, yvel)
             mDragHelper.settleCapturedViewAt(mDragOriLeft, mDragOriTop)
             invalidate()
+            Log.d("jihongwen", "onViewReleased invalidate")
         }
     }
 
@@ -47,8 +109,10 @@ class TestViewGroup @JvmOverloads constructor(
 
     override fun computeScroll() {
         super.computeScroll()
+        Log.d("jihongwen", "computeScroll")
         if (mDragHelper.continueSettling(true)) {
-            invalidate()
+            ViewCompat.postInvalidateOnAnimation(this)
+            Log.d("jihongwen", "computeScroll invalidate")
         }
     }
 
@@ -59,5 +123,21 @@ class TestViewGroup @JvmOverloads constructor(
     override fun onTouchEvent(event: MotionEvent): Boolean {
         mDragHelper.processTouchEvent(event)
         return true
+    }
+
+    private fun smoothSlideTo(finalTop: Int) {
+        if (mDragHelper.smoothSlideViewTo(slidingUpPanel!!, slidingUpPanel!!.left, finalTop)) {
+            ViewCompat.postInvalidateOnAnimation(this)
+        }
+    }
+
+    private fun changeBtn(top: Int) {
+        // 0f~360f
+        val rate = (oriSlidingUpTop - top) / oriSlidingUpTop.toFloat()
+        openBtn?.rotation = rate * 360f
+        closeBtn?.rotation = rate * 360f
+        Log.d("jihongwen", "rotation=" + openBtn?.rotation)
+        openBtn?.alpha = 1 - rate
+        closeBtn?.alpha = rate
     }
 }
